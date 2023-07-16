30c82a5e80275ce367abd761b14f537504fdb0a3
Agustin Acosta <agustinacostaolivera@gmail.com>
2023-07-16 19:04:15 +0200

    Created component and pages folders

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer-container'>
+            <a className='link' href="">LinkedIn</a>
+            <div className='sound'>
+            <p>SOUND</p><button>ON</button>
+            </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='nav-container'>
+            <a className='link' href="">.AgustinWaves</a>
+            <div className='nav-links'>
+                <a className='link' href="#">.ABOUT</a>
+                <a className='link' href="#">.WORK</a>
+                <a className='link' href="#">.CONTACT</a>
+            </div>
+    </div>
+  )
+}
+
+export default Navbar
