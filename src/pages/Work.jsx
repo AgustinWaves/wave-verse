8b31b50e17060782b48e370ef3d60b6e801db341
Agustin Acosta <agustinacostaolivera@gmail.com>
2023-07-17 15:58:43 +0200

    Work page structure

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Work = () => {
   return (
-    <div>Work</div>
+    <div className='work-container'>
+      <div className='title-box'>
+        <h1 className='titles'>.DOOMED</h1>
+      </div>
+      <div className='title-box'>
+        <h1 className='titles'>.LENSES</h1>
+      </div>
+      <div className='title-box'>
+        <h1 className='titles'>.EXMPL</h1>
+      </div>
+      <div className='title-box'>
+        <h1 className='titles'>.EXMPL</h1>
+      </div>
+    </div>
   )
 }
 
