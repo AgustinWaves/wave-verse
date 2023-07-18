fef3038a3dab518de0c801cfa5f717dbfa77dfa8
Agustin Acosta <agustinacostaolivera@gmail.com>
2023-07-18 15:45:11 +0200

    About page layout

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='container'>
+      <div className='about-container'>
+        <h1 className='about-text'>Being curious is what defines me. Although, defining yourself is also limiting yourself at the same time. The fact is, that we are beings in constant movement, constant change; we are not static objects. I always try to make my younger self proud of who I am today. Hard work, making music and enjoying the little things is what makes me happy. The art of capturing moments is something I am always fascinated by.
+        The final fact is, that I'm a nonstop learner. Learning for me equates to exploring new ideas and growing as an individual.</h1>      
+       </div>
+    </div>
   )
 }
 
